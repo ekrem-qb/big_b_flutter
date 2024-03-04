570149428edf483acedef30000b0b82310a5a3be
ekrem-qb <ekrem.bayram@mail.ru>
2024-03-04 03:49:34 +0300

    Add iOS style date and time pickers with ok buton shower functions

---
+++ lib/ui/widgets/extensions/pickers/date_picker.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+Future<DateTime?> showCupertinoDatePicker({
+  required final BuildContext context,
+  required final DateTime initialDate,
+  required final DateTime minimumDate,
+  required final bool showDayOfWeek,
+}) async {
+  return showCupertinoModalPopup<DateTime?>(
+    context: context,
+    builder: (final context) {
+      return _Widget(initialDate: initialDate, minimumDate: minimumDate, showDayOfWeek: showDayOfWeek);
+    },
+  );
+}
+
+class _Model {
+  _Model(this._context, this._date);
+
+  final BuildContext _context;
+
+  DateTime _date;
+
+  void setDate(final DateTime value) {
+    if (_date == value) return;
+
+    _date = value;
+    HapticFeedback.selectionClick();
+  }
+
+  void ok() {
+    Navigator.pop(_context, _date);
+  }
+}
+
+class _Widget extends StatelessWidget {
+  const _Widget({
+    required this.initialDate,
+    required this.minimumDate,
+    required this.showDayOfWeek,
+  });
+
+  final DateTime initialDate;
+  final DateTime minimumDate;
+  final bool showDayOfWeek;
+
+  @override
+  Widget build(final BuildContext context) {
+    return Provider(
+      create: (final context) => _Model(context, initialDate),
+      child: _Picker(initialDate: initialDate, minimumDate: minimumDate, showDayOfWeek: showDayOfWeek),
+    );
+  }
+}
+
+class _Picker extends StatelessWidget {
+  const _Picker({
+    required this.initialDate,
+    required this.minimumDate,
+    required this.showDayOfWeek,
+  });
+
+  final DateTime initialDate;
+  final DateTime minimumDate;
+  final bool showDayOfWeek;
+
+  @override
+  Widget build(final BuildContext context) {
+    final model = context.read<_Model>();
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Flexible(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: FractionallySizedBox(
+              heightFactor: 0.36,
+              child: CupertinoPopupSurface(
+                child: CupertinoDatePicker(
+                  initialDateTime: initialDate,
+                  minimumDate: minimumDate,
+                  showDayOfWeek: showDayOfWeek,
+                  mode: CupertinoDatePickerMode.date,
+                  onDateTimeChanged: model.setDate,
+                ),
+              ),
+            ),
+          ),
+        ),
+        MediaQuery.removePadding(
+          context: context,
+          removeTop: true,
+          child: CupertinoActionSheet(
+            actions: [
+              CupertinoActionSheetAction(
+                isDefaultAction: true,
+                onPressed: model.ok,
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/ui/widgets/extensions/pickers/time_picker.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+Future<Duration?> showCupertinoTimePicker({
+  required final BuildContext context,
+  required final Duration initialTime,
+}) async {
+  return showCupertinoModalPopup<Duration?>(
+    context: context,
+    builder: (final context) {
+      return _Widget(initialTime: initialTime);
+    },
+  );
+}
+
+class _Model {
+  _Model(this._context, this._date);
+
+  final BuildContext _context;
+
+  Duration _date;
+
+  void setTime(final Duration value) {
+    if (_date == value) return;
+
+    _date = value;
+    HapticFeedback.selectionClick();
+  }
+
+  void ok() {
+    Navigator.pop(_context, _date);
+  }
+}
+
+class _Widget extends StatelessWidget {
+  const _Widget({
+    required this.initialTime,
+  });
+
+  final Duration initialTime;
+
+  @override
+  Widget build(final BuildContext context) {
+    return Provider(
+      create: (final context) => _Model(context, initialTime),
+      child: _Picker(initialTime: initialTime),
+    );
+  }
+}
+
+class _Picker extends StatelessWidget {
+  const _Picker({
+    required this.initialTime,
+  });
+
+  final Duration initialTime;
+
+  @override
+  Widget build(final BuildContext context) {
+    final model = context.read<_Model>();
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Flexible(
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: FractionallySizedBox(
+              heightFactor: 0.36,
+              child: CupertinoPopupSurface(
+                child: CupertinoTimerPicker(
+                  mode: CupertinoTimerPickerMode.hm,
+                  initialTimerDuration: initialTime,
+                  onTimerDurationChanged: model.setTime,
+                ),
+              ),
+            ),
+          ),
+        ),
+        MediaQuery.removePadding(
+          context: context,
+          removeTop: true,
+          child: CupertinoActionSheet(
+            actions: [
+              CupertinoActionSheetAction(
+                isDefaultAction: true,
+                onPressed: model.ok,
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
