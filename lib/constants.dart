8837a121543d418891d36747b8db5fd06eefd7fe
ekrem-qb <ekrem.bayram@mail.ru>
2024-03-13 19:22:44 +0300

    Made isDesktop bool platform const

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+@pragma('vm:platform-const')
 final isDesktop = !kIsWeb && (Platform.isLinux || Platform.isMacOS || Platform.isWindows);
 final skippedFocusNode = FocusNode(canRequestFocus: false, skipTraversal: true);
 
