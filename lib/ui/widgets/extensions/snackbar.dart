0ae653bd1f22001127d789f9045960cd7e3b66a3
ekrem-qb <ekrem.bayram@mail.ru>
2024-05-02 03:30:14 +0300

    Add printing error send to snackbar in debug mode

--- lib/ui/widgets/extensions/snackbar.dart
+++ lib/ui/widgets/extensions/snackbar.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void showSnackbar({required final String text, required final BuildContext context}) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(text)));
+  if (kDebugMode) {
+    print(text);
+  }
 }
