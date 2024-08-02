f8017800e407e470db3079981c00f995ea0e46fb
ekrem-qb <ekrem.bayram@mail.ru>
2024-08-02 20:07:14 +0300

    Remove Auto Router going back from Mouse Navigator

--- lib/ui/widgets/extensions/mouse_navigator.dart
+++ lib/ui/widgets/extensions/mouse_navigator.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/widgets.dart';
-import 'package:provider/provider.dart';
-
-import '../../app_router/app_router.dart';
 
 class MouseNavigator extends StatelessWidget {
   const MouseNavigator({required this.child, super.key});
@@ -12,14 +9,12 @@ class MouseNavigator extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
-    final router = context.read<AppRouter?>();
-
     return kIsWeb
         ? child
         : Listener(
             onPointerDown: (final pointerEvent) {
               if (pointerEvent.buttons == kBackMouseButton) {
-                (router?.navigationHistory.length ?? 0) > 2 ? router?.back() : Navigator.maybePop(context);
+                Navigator.maybePop(context);
               }
             },
             child: child,
