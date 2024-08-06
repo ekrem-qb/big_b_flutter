c3df12265739e8dc54bd7536371c2c3204210e2a
ekrem-qb <ekrem.bayram@mail.ru>
2024-08-06 22:04:14 +0300

    Add Dialog to route name replacement

--- lib/ui/app_router/app_router.dart
+++ lib/ui/app_router/app_router.dart
@@ -12,7 +12,7 @@ import '../widgets/tabs/tasks/tasks_page.dart';
 
 part 'app_router.gr.dart';
 
-@AutoRouterConfig()
+@AutoRouterConfig(replaceInRouteName: 'Page|Screen|Dialog,Route')
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes {
