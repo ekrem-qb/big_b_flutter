d04d679bd383e2c2a75f38bf123c039e08f68d6d
ekrem-qb <ekrem.bayram@mail.ru>
2024-08-15 20:26:48 +0300

    Rewrite More page to maintain design same as Tasks

--- lib/ui/widgets/tabs/more/more_page.dart
+++ lib/ui/widgets/tabs/more/more_page.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../extensions/app_bar_controller.dart';
 import '../../pages/app/bloc/app_bloc.dart';
 
 @RoutePage()
@@ -10,15 +11,31 @@ class MorePage extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
-    return Column(
-      children: [
-        AppBar(
-          actions: const [
-            _LogoutButton(),
-          ],
-          elevation: 4,
-        ),
-      ],
+    return ChangeNotifierProvider(
+      create: (final _) => AppBarController(),
+      child: const MoreView(),
+    );
+  }
+}
+
+class MoreView extends StatelessWidget {
+  const MoreView({
+    super.key,
+  });
+
+  @override
+  Widget build(final BuildContext context) {
+    final appBar = context.read<AppBarController>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gene'),
+        notificationPredicate: appBar.onScroll,
+        actions: const [
+          _LogoutButton(),
+          SizedBox(width: 8),
+        ],
+      ),
     );
   }
 }
@@ -29,10 +46,24 @@ class _LogoutButton extends StatelessWidget {
   @override
   Widget build(final BuildContext context) {
     final model = context.read<AppBloc>();
+    late final AppBarController appBar;
+    var isInitialized = false;
+    context.select((final AppBarController newBloc) {
+      if (!isInitialized) {
+        appBar = newBloc;
+        isInitialized = true;
+      }
+      return appBar.isScrolled;
+    });
 
-    return ElevatedButton(
+    return ElevatedButton.icon(
+      style: ButtonStyle(
+        visualDensity: VisualDensity.standard,
+        elevation: appBar.isScrolled ? const WidgetStatePropertyAll(0) : null,
+      ),
       onPressed: () => model.add(const AppEventSignOutRequested()),
-      child: const Text('Çıkış'),
+      icon: const Icon(Icons.logout),
+      label: const Text('Çıkış'),
     );
   }
 }
