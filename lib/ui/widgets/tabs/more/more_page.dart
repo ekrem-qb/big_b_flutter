435ac5679f3e9e2c5a531d922a64e4b1955ace08
ekrem-qb <ekrem.bayram@mail.ru>
2024-08-16 01:49:46 +0300

    Add Rules page to more menu

--- lib/ui/widgets/tabs/more/more_page.dart
+++ lib/ui/widgets/tabs/more/more_page.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../app_router/app_router.dart';
 import '../../extensions/app_bar_controller.dart';
 import '../../pages/app/bloc/app_bloc.dart';
 
@@ -36,6 +37,23 @@ class MoreView extends StatelessWidget {
           SizedBox(width: 8),
         ],
       ),
+      body: const Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Wrap(
+            spacing: 8,
+            runSpacing: 8,
+            children: [
+              _Button(
+                icon: Icons.rule_rounded,
+                label: 'Kurallar',
+                route: RulesRoute(),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
@@ -64,6 +82,42 @@ class _LogoutButton extends StatelessWidget {
       onPressed: () => model.add(const AppEventSignOutRequested()),
       icon: const Icon(Icons.logout),
       label: const Text('Çıkış'),
+    );
+  }
+}
+
+class _Button extends StatelessWidget {
+  const _Button({
+    required this.icon,
+    required this.label,
+    required this.route,
+  });
+
+  final IconData icon;
+  final String label;
+  final PageRouteInfo route;
+
+  @override
+  Widget build(final BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+      ),
+      onPressed: () => context.pushRoute(route),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: Column(
+          children: [
+            Icon(icon),
+            const SizedBox(height: 8),
+            Text(label),
+          ],
+        ),
+      ),
     );
   }
 }
