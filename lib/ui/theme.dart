7dbd8c1b30c02c84c8d6171fdf844c25c05ca479
ekrem-qb <ekrem.bayram@mail.ru>
2024-08-19 22:13:43 +0300

    Add default radius const to theme file

--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -5,9 +5,10 @@ const scrollCurve = Curves.easeOutExpo;
 const largeTextStyle = TextStyle(fontSize: 32, fontWeight: FontWeight.bold, letterSpacing: 0, height: 1.25);
 const textStyle = TextStyle(fontSize: 22, fontWeight: FontWeight.w400, letterSpacing: 0, height: 1.27);
 const smallTextStyle = TextStyle(fontSize: 16, fontWeight: FontWeight.w500, letterSpacing: 0.15, height: 1.50);
+const kDefaultRadius = BorderRadius.all(Radius.circular(12));
 
 const kDefaultShape = RoundedRectangleBorder(
-  borderRadius: BorderRadius.all(Radius.circular(12)),
+  borderRadius: kDefaultRadius,
 );
 
 const _appBarTheme = AppBarTheme(
