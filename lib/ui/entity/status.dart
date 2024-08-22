d33244430af01726b025c1bedb4e02bbbf39de9c
ekrem-qb <ekrem.bayram@mail.ru>
2024-08-22 20:24:59 +0300

    Add StatusOf class

--- lib/ui/entity/status.dart
+++ lib/ui/entity/status.dart
@@ -12,3 +12,13 @@ sealed class Status with _$Status {
   const factory Status.completed() = StatusCompleted;
 }
 
+@freezed
+sealed class StatusOf<T> with _$StatusOf<T> {
+  const factory StatusOf.data({
+    required final T data,
+  }) = StatusOfData;
+  const factory StatusOf.loading() = StatusOfLoading;
+  const factory StatusOf.error({
+    required final String error,
+  }) = StatusOfError;
+}
--- lib/ui/entity/status.freezed.dart
+++ lib/ui/entity/status.freezed.dart
@@ -244,3 +244,229 @@ abstract class StatusCompleted implements Status {
   const factory StatusCompleted() = _$StatusCompletedImpl;
 }
 
+/// @nodoc
+mixin _$StatusOf<T> {}
+
+/// @nodoc
+abstract class $StatusOfCopyWith<T, $Res> {
+  factory $StatusOfCopyWith(
+          StatusOf<T> value, $Res Function(StatusOf<T>) then) =
+      _$StatusOfCopyWithImpl<T, $Res, StatusOf<T>>;
+}
+
+/// @nodoc
+class _$StatusOfCopyWithImpl<T, $Res, $Val extends StatusOf<T>>
+    implements $StatusOfCopyWith<T, $Res> {
+  _$StatusOfCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of StatusOf
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+abstract class _$$StatusOfDataImplCopyWith<T, $Res> {
+  factory _$$StatusOfDataImplCopyWith(_$StatusOfDataImpl<T> value,
+          $Res Function(_$StatusOfDataImpl<T>) then) =
+      __$$StatusOfDataImplCopyWithImpl<T, $Res>;
+  @useResult
+  $Res call({T data});
+}
+
+/// @nodoc
+class __$$StatusOfDataImplCopyWithImpl<T, $Res>
+    extends _$StatusOfCopyWithImpl<T, $Res, _$StatusOfDataImpl<T>>
+    implements _$$StatusOfDataImplCopyWith<T, $Res> {
+  __$$StatusOfDataImplCopyWithImpl(
+      _$StatusOfDataImpl<T> _value, $Res Function(_$StatusOfDataImpl<T>) _then)
+      : super(_value, _then);
+
+  /// Create a copy of StatusOf
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? data = freezed,
+  }) {
+    return _then(_$StatusOfDataImpl<T>(
+      data: freezed == data
+          ? _value.data
+          : data // ignore: cast_nullable_to_non_nullable
+              as T,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$StatusOfDataImpl<T> implements StatusOfData<T> {
+  const _$StatusOfDataImpl({required this.data});
+
+  @override
+  final T data;
+
+  @override
+  String toString() {
+    return 'StatusOf<$T>.data(data: $data)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$StatusOfDataImpl<T> &&
+            const DeepCollectionEquality().equals(other.data, data));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(data));
+
+  /// Create a copy of StatusOf
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$StatusOfDataImplCopyWith<T, _$StatusOfDataImpl<T>> get copyWith =>
+      __$$StatusOfDataImplCopyWithImpl<T, _$StatusOfDataImpl<T>>(
+          this, _$identity);
+}
+
+abstract class StatusOfData<T> implements StatusOf<T> {
+  const factory StatusOfData({required final T data}) = _$StatusOfDataImpl<T>;
+
+  T get data;
+
+  /// Create a copy of StatusOf
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$StatusOfDataImplCopyWith<T, _$StatusOfDataImpl<T>> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class _$$StatusOfLoadingImplCopyWith<T, $Res> {
+  factory _$$StatusOfLoadingImplCopyWith(_$StatusOfLoadingImpl<T> value,
+          $Res Function(_$StatusOfLoadingImpl<T>) then) =
+      __$$StatusOfLoadingImplCopyWithImpl<T, $Res>;
+}
+
+/// @nodoc
+class __$$StatusOfLoadingImplCopyWithImpl<T, $Res>
+    extends _$StatusOfCopyWithImpl<T, $Res, _$StatusOfLoadingImpl<T>>
+    implements _$$StatusOfLoadingImplCopyWith<T, $Res> {
+  __$$StatusOfLoadingImplCopyWithImpl(_$StatusOfLoadingImpl<T> _value,
+      $Res Function(_$StatusOfLoadingImpl<T>) _then)
+      : super(_value, _then);
+
+  /// Create a copy of StatusOf
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+
+class _$StatusOfLoadingImpl<T> implements StatusOfLoading<T> {
+  const _$StatusOfLoadingImpl();
+
+  @override
+  String toString() {
+    return 'StatusOf<$T>.loading()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$StatusOfLoadingImpl<T>);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class StatusOfLoading<T> implements StatusOf<T> {
+  const factory StatusOfLoading() = _$StatusOfLoadingImpl<T>;
+}
+
+/// @nodoc
+abstract class _$$StatusOfErrorImplCopyWith<T, $Res> {
+  factory _$$StatusOfErrorImplCopyWith(_$StatusOfErrorImpl<T> value,
+          $Res Function(_$StatusOfErrorImpl<T>) then) =
+      __$$StatusOfErrorImplCopyWithImpl<T, $Res>;
+  @useResult
+  $Res call({String error});
+}
+
+/// @nodoc
+class __$$StatusOfErrorImplCopyWithImpl<T, $Res>
+    extends _$StatusOfCopyWithImpl<T, $Res, _$StatusOfErrorImpl<T>>
+    implements _$$StatusOfErrorImplCopyWith<T, $Res> {
+  __$$StatusOfErrorImplCopyWithImpl(_$StatusOfErrorImpl<T> _value,
+      $Res Function(_$StatusOfErrorImpl<T>) _then)
+      : super(_value, _then);
+
+  /// Create a copy of StatusOf
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? error = null,
+  }) {
+    return _then(_$StatusOfErrorImpl<T>(
+      error: null == error
+          ? _value.error
+          : error // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$StatusOfErrorImpl<T> implements StatusOfError<T> {
+  const _$StatusOfErrorImpl({required this.error});
+
+  @override
+  final String error;
+
+  @override
+  String toString() {
+    return 'StatusOf<$T>.error(error: $error)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$StatusOfErrorImpl<T> &&
+            (identical(other.error, error) || other.error == error));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, error);
+
+  /// Create a copy of StatusOf
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$StatusOfErrorImplCopyWith<T, _$StatusOfErrorImpl<T>> get copyWith =>
+      __$$StatusOfErrorImplCopyWithImpl<T, _$StatusOfErrorImpl<T>>(
+          this, _$identity);
+}
+
+abstract class StatusOfError<T> implements StatusOf<T> {
+  const factory StatusOfError({required final String error}) =
+      _$StatusOfErrorImpl<T>;
+
+  String get error;
+
+  /// Create a copy of StatusOf
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$StatusOfErrorImplCopyWith<T, _$StatusOfErrorImpl<T>> get copyWith =>
+      throw _privateConstructorUsedError;
+}
