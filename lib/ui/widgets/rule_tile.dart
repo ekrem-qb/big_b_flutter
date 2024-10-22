a4410ff54b4579f546e57ca7d92a19ddbe45d6e7
ekrem-qb <ekrem.bayram@mail.ru>
2024-10-22 03:36:06 +0300

    Separate RuleText and RuleIcon from RuleTile

--- lib/ui/widgets/rule_tile.dart
+++ lib/ui/widgets/rule_tile.dart
@@ -14,24 +14,52 @@ class RuleTile extends StatelessWidget {
   Widget build(final BuildContext context) {
     return Row(
       children: [
-        Icon(
-          switch (type) {
-            const (WordsRule) => Icons.abc,
-            const (NameRule) => Icons.person,
-            _ => Icons.question_mark,
-          },
-        ),
+        RuleIcon(type: type),
         const SizedBox(width: 8),
-        Text(
-          switch (type) {
-            const (WordsRule) => 'Çalışan bu kelimeleri kullanıyor mu?',
-            const (NameRule) => 'Çalışan kendi adını söyledi mi?',
-            _ => 'Özel',
-          },
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-        ),
+        RuleText(type: type),
       ],
+    );
+  }
+}
+
+class RuleIcon extends StatelessWidget {
+  const RuleIcon({
+    required this.type,
+    super.key,
+  });
+
+  final Type type;
+
+  @override
+  Widget build(final BuildContext context) {
+    return Icon(
+      switch (type) {
+        const (WordsRule) => Icons.abc,
+        const (NameRule) => Icons.person,
+        _ => Icons.auto_awesome,
+      },
+    );
+  }
+}
+
+class RuleText extends StatelessWidget {
+  const RuleText({
+    required this.type,
+    super.key,
+  });
+
+  final Type type;
+
+  @override
+  Widget build(final BuildContext context) {
+    return Text(
+      switch (type) {
+        const (WordsRule) => 'Çalışan bu kelimeleri kullanıyor mu?',
+        const (NameRule) => 'Çalışan kendi adını söyledi mi?',
+        _ => 'Özel',
+      },
+      maxLines: 1,
+      overflow: TextOverflow.ellipsis,
     );
   }
 }
