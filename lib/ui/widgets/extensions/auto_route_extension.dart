6af297c5874747a529a3e5b5bc4418e187a9f2cf
ekrem-qb <ekrem.bayram@mail.ru>
2024-11-15 21:17:37 +0300

    Add Auto Route extension with navigateAll method

---
+++ lib/ui/widgets/extensions/auto_route_extension.dart
@@ -0,0 +1,19 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/widgets.dart';
+
+extension AutoRouteExtension on TabsRouter {
+  Future<void> navigateAll(final List<PageRouteInfo> routes) async {
+    final firstRoute = routes.first;
+    final path = matcher.buildPathTo(firstRoute)?.path;
+    if (path != null && (stackRouterOfIndex(activeIndex)?.currentPath.contains(path) ?? false)) {
+      for (var i = 0; i < routes.length; i++) {
+        await navigate(routes[i]);
+      }
+    } else {
+      await navigate(firstRoute);
+      WidgetsBinding.instance.addPostFrameCallback((final _) {
+        stackRouterOfIndex(activeIndex)?.pushAll(routes.sublist(1));
+      });
+    }
+  }
+}
